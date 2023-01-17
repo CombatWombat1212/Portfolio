5919b6c73f3dd0ef412cdb126cdb54f22c72ed46
ItsMeSamG <itsmesamg123@gmail.com>
2023-01-17 16:40:46 -0500

    Gettin started:)

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 
 
+import { v4 as uuidv4 } from "uuid";
+
+
 
 function Index() {
 
 
   return (
     <>
+
+
+    <div className="hero">
+      <div className="hero--desc">
+        <h1>Hi, I'm Sam Giustizia,<br></br>how are you?</h1>
+        <p className="t--h3">I'm a multidisciplinary designer <br></br>who solves real-world problems<br></br>with digital-world solutions.</p>
+        <p className="t--body">This is my portfolio.  Enjoy your stay :)</p>
+      </div>
+      <div className="hero--graphic"></div>
+    </div>
+
+
 
     </>
   );
