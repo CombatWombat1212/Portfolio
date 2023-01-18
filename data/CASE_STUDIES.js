31931ae6402de69613c988481c0edb27b1f6dc81
ItsMeSamG <itsmesamg123@gmail.com>
2023-01-18 09:59:08 -0500

    First graphic in

---
+++ data/CASE_STUDIES.js
@@ -0,0 +1,20 @@
+const CASE_STUDIES = [
+  {
+    name: "MakeRight",
+    description: "Translating The Benefits of 3D Printing to Everyday Consumers.",
+    tags: ["Thesis", "UX/UI Design", "User Research"],
+  },
+  {
+    name: "Made Clothing Co.",
+    description: "Product Rendering for a Toronto-Based Men’s Fashion Brand",
+    tags: ["Client", "3D Design", "Product Rendering"],
+  },
+  {
+    name: "KoalaKo",
+    description: "Supporting Parents in Fostering Adolescent Creative Development",
+    tags: ["UX/UI Design", "User Research", "Team Project"],
+  },
+];
+
+
+export default CASE_STUDIES;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,30 @@
-
-
 import { v4 as uuidv4 } from "uuid";
+import Image from "next/image";
 
-
+import CASE_STUDIES from "/data/CASE_STUDIES";
 
 function Index() {
-
-
   return (
     <>
-
-
-    <div className="hero">
-      <div className="hero--desc">
-        <h1>Hi, I'm Sam Giustizia,<br></br>how are you?</h1>
-        <p className="t--h3">I'm a multidisciplinary designer <br></br>who solves real-world problems<br></br>with digital-world solutions.</p>
-        <p className="t--body">This is my portfolio.  Enjoy your stay :)</p>
+      <div className="container hero">
+        <div className="hero--desc col-6">
+          <h1 className="hero--title">
+            Hi, I'm <span className="hero--name">Sam Giustizia</span>,<br></br>how are you?
+          </h1>
+          <p className="text--h3">
+            I'm a multidisciplinary designer,<br></br>
+            and a creator of digital solutions,<br></br>
+            tailor-fitted to real-world problems.
+          </p>
+          <p>This is my portfolio. Enjoy your stay :)</p>
+        </div>
+        <div className="hero--graphic col-5">
+          <div className="hero--img">
+            {/* TODO: fix the pixelated look of this img */}
+            <Image src="/assets/images/flair/Arrow_Squiggle.png" alt="Squiggly arrow pointing downwards" width={651} height={1003} />
+          </div>
+        </div>
       </div>
-      <div className="hero--graphic"></div>
-    </div>
-
-
-
     </>
   );
 }
