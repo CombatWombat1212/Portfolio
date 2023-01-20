208964dd0d52cdcdf0a1f61aab30d0e15d223fcd
ItsMeSamG <itsmesamg123@gmail.com>
2023-01-20 17:11:40 -0500

    BIG progress, wrapped up the footer and got started on the case study pages

--- pages/Studies/MakeRight.js
+++ pages/Studies/MakeRight.js
@@ -1,10 +1,19 @@
+import { StudyPanel } from "/components/elements/Panel";
+import getStudy from "../../scripts/GetStudy";
+import CaseStudyPage from "@/components/studies/CaseStudyPage";
+
 function MakeRight() {
-    return (
-      <>
-        <>MakeRight</>
-      </>
-    );
-  }
-  
-  export default MakeRight;
-  
+  const study = getStudy();
+
+  return (
+    <>
+      <CaseStudyPage id={study.id}>
+
+          <StudyPanel variant="study" study={study} />
+
+       </CaseStudyPage>
+    </>
+  );
+}
+
+export default MakeRight;
