7673a44cde7a33d0a62e1d0aac94401eeac730af
ItsMeSamG <itsmesamg123@gmail.com>
2023-02-22 16:54:56 -0500

    about to gut stuff i didn't need

--- components/sections/Pitch.js
+++ components/sections/Pitch.js
@@ -3,19 +3,13 @@ import { addClassToJsxObj } from "./sections_utilities/ClassUtilities";
 import { getSectionChildren } from "./sections_utilities/GetSectionChildren";
 import MAKERIGHT_IMGS from "@/data/MAKERIGHT_IMGS";
 import Section from "./Sections";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useMountEffect } from "@/scripts/hooks/useMountEffect";
 import { RESIZE_TIMEOUT, splitPx } from "@/scripts/GlobalUtilities";
 const laptop_frame = MAKERIGHT_IMGS.pitch_laptop_frame;
 
-
-
-
 // TODO: i think you should do the same animation on the description side to make it have smooth scrolling transitions too? Or maybe something to lock it in place better next to the screen? might not be priority though
 // TODO: how tf is this gonna be responsive?? LOL good luck with that (nah u got this)
-
-
-
 
 var pitches = [];
 
@@ -32,25 +26,63 @@ function PitchItem(pitch) {
     height: 0,
     width: 0,
     elem: this.elem.querySelector(".pitch--screens"),
-  }
-  this.rows = {
-    elems: Array.from(this.elem.querySelectorAll(".pitch--row.pitch--explaination")),
-    current: 0,
-    previous: null, // Add a variable to track the previous row
   };
+  this.rows = {
+    elems: Array.from(this.elem.querySelectorAll(".pitch--placeholder")),
+    current: 0,
+    previous: null,
+  };
+  this.captions = {
+    height: 0,
+    width: 0,
+    elem: this.elem.querySelector(".pitch--captions"),
+    children: captionsChildrenInit(Array.from(this.elem.querySelectorAll(".pitch--captions"))),
+  };
+
+  
+  function captionsChildrenInit(arr) {
+    var objs = [];
+    for (let i = 0; i < arr.length; i++) {
+      const child = arr[i];
+      var obj = {
+        height: 0,
+        width: 0,
+        elems: child,
+        children: captionsInnerInit(Array.from(child.children)),
+      };
+      objs.push(obj);
+    }
+    return objs;
+  }
+
+  function captionsInnerInit(arr) {
+    var objs = [];
+    for (let i = 0; i < arr.length; i++) {
+      const child = arr[i];
+      var obj = {
+        height: 0,
+        width: 0,
+        elems: child,
+        children: captionsInnerInit(Array.from(child.children)),
+      };
+      objs.push(obj);
+    }
+    return objs;
+  }
+
+  console.log(this);
 }
 
-
-
-
-
-function pitchSetCurrentRow(pitch){
+function pitchSetCurrentRow(pitch) {
   var elem = pitch.elem;
   elem.style.setProperty("--pitch-current-row", pitch.rows.current);
+
+  var { current, previous } = pitch.rows;
+  if (current !== previous) {
+    pitch.rows.previous = current;
+    pitchSetCurrentRow(pitch);
+  }
 }
-
-
-
 
 function pitchGetCurrentRow(pitch) {
   // Get the rows array and the current row index
@@ -74,56 +106,97 @@ function pitchGetCurrentRow(pitch) {
     }
     pitch.rows.current = maxIndex;
   }
-
-  // Check if the current row is different from the previous row
-  if (current !== previous) {
-    pitch.rows.previous = current;
-    pitchSetCurrentRow(pitch);
-  }
 }
 
-
-
-function pitchGetInView(pitch){
-
+function pitchGetInView(pitch) {
   var elem = pitch.elem;
   var rect = elem.getBoundingClientRect();
   var inView = rect.top < window.innerHeight && rect.bottom > 0;
   if (inView) pitch.inView = true;
   else pitch.inView = false;
-
 }
-
-
-
 
 function pitchSetRowSize(pitch) {
   var elem = pitch.elem;
   var height = pitch.frame.height;
   var width = pitch.frame.width;
+
+  // Set the row height and width
   elem.style.setProperty("--pitch-row-height", height + "px");
   elem.style.setProperty("--pitch-row-width", width + "px");
 
+  // Set the row overflow buffer
   elem.style.setProperty("--pitch-overflow-buffer-x", height - pitch.screens.height + "px");
   elem.style.setProperty("--pitch-overflow-buffer-y", width - pitch.screens.width + "px");
+
+
+  // Set the caption height and width
+  elem.style.setProperty("--pitch-caption-height", pitch.captions.height + "px");
+  elem.style.setProperty("--pitch-caption-width", pitch.captions.width + "px");
+
+  // Set the caption inner height and width
+  var placeholders = Array.from(elem.querySelectorAll(".pitch--placeholder"));
+  for (let i = 0; i < pitch.captions.children.length; i++) {
+    const child = pitch.captions.children[i];
+    for (let j = 0; j < child.children.length; j++) {
+      const child2 = child.children[j];
+      child2.elems.style.setProperty("--pitch-caption-inner-height", child2.height + "px");
+      placeholders[j].style.setProperty("--pitch-caption-inner-height", child2.height + "px");
+    }
+  }
+
 
 }
 
 function pitchGetRowSize(pitch) {
   var elem = pitch.elem;
   var frame = elem.querySelector(".pitch--laptop");
+
+  // get the height and width of the frame
   var height = splitPx(window.getComputedStyle(frame).height);
   var width = splitPx(window.getComputedStyle(frame).width);
   pitch.frame.height = height;
   pitch.frame.width = width;
 
+  // get width and height of the screens
   var screens = pitch.screens.elem;
   var screensHeight = splitPx(window.getComputedStyle(screens).height);
   var screensWidth = splitPx(window.getComputedStyle(screens).width);
   pitch.screens.height = screensHeight;
   pitch.screens.width = screensWidth;
-}
 
+
+
+  // width from this point on isnt needed
+
+
+  // get width and height of the captions, same as the frame
+  for (let i = 0; i < pitch.captions.children.length; i++) {
+    const child = pitch.captions.children[i];
+    child.height = pitch.frame.height;
+  }
+
+  
+  // get width and height of the captions inner
+  for (let i = 0; i < pitch.captions.children.length; i++) {
+    const child = pitch.captions.children[i];
+    for (let j = 0; j < child.children.length; j++) {
+      const innerChild = child.children[j];
+
+      var h = 0;
+      for (let k = 0; k < innerChild.children.length; k++) {
+        const innerInnerChild = innerChild.children[k];
+        var captions = innerInnerChild.elems;
+        var captionsHeight = splitPx(window.getComputedStyle(captions).height);
+        captionsHeight += splitPx(window.getComputedStyle(captions).marginTop);
+        captionsHeight += splitPx(window.getComputedStyle(captions).marginBottom);
+        h += captionsHeight;
+      }
+      innerChild.height = h;
+    }
+  }
+  
+}
 
 function pitchInit(pitch) {
   pitch = pitch.current ? pitch.current : pitch;
@@ -177,20 +250,26 @@ function Pitch({ children }) {
           <Laptop rows={rows} />
         </div>
         <div className="pitch--column col-3">
-
+          <div className="pitch--captions">
             {rows.map((row, i) => {
               var { description, title, heading, graphic, other, vector, mockup } = formatRow(row);
               var vectorProps = vector.props;
-  
+
               return (
-                <div className="pitch--row pitch--explaination" key={i}>
+                <div className="pitch--row pitch--body" key={i}>
                   <Graphic {...vectorProps} />
                   {heading && <>{heading}</>}
                   {description && <>{description}</>}
                 </div>
               );
             })}
-
+          </div>
+          <div className="pitch--empties">
+            {rows.map((row, i) => {
+              var { description, title, heading, graphic, other, vector, mockup } = formatRow(row);
+              return <div key={i} className="pitch--row pitch--placeholder"></div>;
+            })}
+          </div>
         </div>
       </div>
     </>
@@ -216,48 +295,16 @@ function formatRow(row) {
   };
 }
 
-
-
-
-
-function pitchScroll(e){
-
-  
-  
+function pitchScroll(e) {
   // detect if pitch.elem is in view and console log it
   pitches.forEach((pitch) => {
     pitchGetInView(pitch);
     if (!pitch.inView) return;
 
     pitchGetCurrentRow(pitch);
-    
-    
-    
+    pitchSetCurrentRow(pitch);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
 
 var pitchIsResizing;
 
@@ -272,8 +319,5 @@ function pitchResizeFunctions() {
     pitchSetRowSize(pitch);
   });
 }
-
-
-
 
 export default Pitch;
