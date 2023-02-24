68efda7637d8a162d5c7560b1364d86893c4993f
ItsMeSamG <itsmesamg123@gmail.com>
2023-02-24 13:18:20 -0500

    changing approach again<3

--- components/studies/Indicator.js
+++ components/studies/Indicator.js
@@ -1,3 +1,4 @@
+import toggle, { dualTransitionToggleOff, dualTransitionToggleOn, simpleToggleOff, simpleToggleOn } from "@/scripts/AnimationTools";
 import { RESIZE_TIMEOUT, splitPx } from "@/scripts/GlobalUtilities";
 import { useMountEffect } from "@/scripts/hooks/useMountEffect";
 import { useEffect, useRef, useState } from "react";
@@ -5,6 +6,7 @@ import { useEffect, useRef, useState } from "react";
 // TODO: fix scss system so theres no gaps between chapters
 
 var globChapters;
+
 
 function Chapter(elem, index, chapters, setChapters) {
   this.elem = elem;
@@ -31,13 +33,11 @@ function Chapter(elem, index, chapters, setChapters) {
   this.observer.observe(elem);
 }
 
-
 function Name(elem) {
   this.chapter = elem;
   this.index = 0;
   this.name = elem?.getAttribute("name") || "";
 }
-
 
 function namesAddElems(names) {
   var nameElems = document.querySelectorAll(".indicator .name--chapter");
@@ -49,9 +49,7 @@ function namesAddElems(names) {
   return n;
 }
 
-
-
-function namesGet(chapters){
+function namesGet(chapters) {
   var n = [];
   chapters.chapters.forEach((chapter) => {
     var name = new Name(chapter.elem);
@@ -61,9 +59,7 @@ function namesGet(chapters){
   return n;
 }
 
-
-
-function namesInit(chapters, setNames, setNamesInitialized){
+function namesInit(chapters, setNames, setNamesInitialized) {
   var n = namesGet(chapters);
   setNames(n);
   setNamesInitialized(true);
@@ -116,16 +112,6 @@ function indicatorGetProgress(touching) {
   return progress;
 }
 
-function chaptersGetUpdatedHeights(obj, chapter) {
-  var newHeight = splitPx(window.getComputedStyle(chapter.elem).height);
-  const updatedChapters = [...obj.chapters];
-  updatedChapters[chapter.index].height = newHeight;
-  const updatedObj = { ...obj, chapters: updatedChapters };
-  obj.chapters = updatedChapters;
-  return updatedObj;
-}
-
-
 function chaptersInit(obj, chapters, setChapters) {
   var all = document.querySelectorAll(".chapter--wrapper");
   obj.elems = Array.from(all);
@@ -137,13 +123,35 @@ function chaptersInit(obj, chapters, setChapters) {
   window.addEventListener("resize", () => indicatorOnResize(obj, setChapters));
 }
 
+function indicatorSetVisibility(progress) {
+  var indicator = document.querySelector(".indicator--wrapper");
+  if (!indicator) return;
+}
+
+
+function indicatorSetSize(indicator, size){
+  indicator.style.setProperty("--indicator-width", `${size.width}px`);
+  indicator.style.setProperty("--indicator-height", `${size.height}px`);
+}
+
+
+function indicatorGetSize(indicator){
+  var width = splitPx(window.getComputedStyle(indicator).width);
+  var height = splitPx(window.getComputedStyle(indicator).height);
+  return {width: width, height: height};
+}
+
+
+
+
 
 function indicatorInit(indicator, chapters, setChapters) {
   var obj = {};
   chaptersInit(obj, chapters, setChapters);
   setChapters(obj);
 
-  // indicatorOnResizeFunctions(obj, setChapters)
+  indicatorOnScroll();
+  indicatorOnResizeFunctions(obj, setChapters);
 }
 
 function Indicator({}) {
@@ -163,43 +171,39 @@ function Indicator({}) {
     namesInit(chapters, setNames, setNamesInitialized);
   }, [chapters, namesInitialized]);
 
-
   useEffect(() => {
     if (!namesInitialized) return;
     var n = namesAddElems(names);
     setNames(n);
   }, [namesInitialized]);
 
-
   useEffect(() => {
     console.log(names);
   }, [names]);
 
-  
   return (
-    <div className="indicator--wrapper" ref={indicator}>
-      <div className="indicator">
-        <div className="name">
-          <span className="name--empty"></span>
-          {namesInitialized && names.map((n) => {
-            return (
-              <span
-                key={n.index}
-                className="name--chapter"
-                style={{
-                  "--name-index": n.index,
-                }}>
-                {n.name}
-              </span>
-            );
-          })}
+    <div className="indicator--wrapper indicator--wrapper__off" ref={indicator}>
+        <div className="indicator indicator__hidden">
+          <div className="name">
+            <span className="name--empty"></span>
+            {namesInitialized &&
+              names.map((n) => {
+                return (
+                  <span
+                    key={n.index}
+                    className="name--chapter"
+                    style={{
+                      "--name-index": n.index,
+                    }}>
+                    {n.name}
+                  </span>
+                );
+              })}
         </div>
       </div>
     </div>
   );
 }
-
-
 
 function indicatorOnScroll(e) {
   // TODO: These calculations are honestly so extra, you don't need to do this based on the exact position of the chapter, and whether or not its overlapping text, you could probably just do it based on the chapter's position on screen i think?  like the whole idea of it needing to be an exact float seems like much
@@ -213,9 +217,8 @@ function indicatorOnScroll(e) {
   var touching = indicatorGetTouching(chapters, name);
   var progress = indicatorGetProgress(touching);
   name.style.setProperty("--chapter-progress", progress);
-  
-  
 
+  indicatorSetVisibility(progress);
 }
 
 var indicatorIsResizing;
@@ -226,6 +229,11 @@ function indicatorOnResize(obj, setChapters) {
 }
 
 function indicatorOnResizeFunctions(obj, setChapters) {
+
+  var indicator = document.querySelector(".indicator--wrapper");
+  var indicatorSize = indicatorGetSize(indicator);
+  indicatorSetSize(indicator, indicatorSize);
+
   const updatedChapters = obj.chapters.map((chapter) => {
     const newHeight = splitPx(window.getComputedStyle(chapter.elem).height);
     return { ...chapter, height: newHeight };
--- scripts/AnimationTools.js
+++ scripts/AnimationTools.js
@@ -10,6 +10,11 @@ function getTransition(elem) {
 }
 
 function simpleToggleOn(elem, str, ov, transitionTime) {
+  var selectorPrefix = "__";
+  if (str == "") selectorPrefix = "";
+
+  str = str + selectorPrefix;
+
   if (!tran) {
     tran = getTransition(elem);
   }
@@ -35,6 +40,11 @@ function simpleToggleOn(elem, str, ov, transitionTime) {
 }
 
 function simpleToggleOff(elem, str, ov, transitionTime) {
+  var selectorPrefix = "__";
+  if (str == "") selectorPrefix = "";
+
+  str = str + selectorPrefix;
+
   if (transitionTime != tran) {
     transitionTime = getTransition(elem);
   }
@@ -56,6 +66,11 @@ function simpleToggleOff(elem, str, ov, transitionTime) {
 }
 
 function dualTransitionToggleOn(elem, str, ov, transitionTime) {
+  var selectorPrefix = "__";
+  if (str == "") selectorPrefix = "";
+
+  str = str + selectorPrefix;
+
   if (transitionTime != tran) {
     transitionTime = getTransition(elem);
   }
@@ -76,6 +91,11 @@ function dualTransitionToggleOn(elem, str, ov, transitionTime) {
 }
 
 function dualTransitionToggleOff(elem, str, ov, transitionTime) {
+  var selectorPrefix = "__";
+  if (str == "") selectorPrefix = "";
+
+  str = str + selectorPrefix;
+
   if (transitionTime != tran) {
     transitionTime = getTransition(elem);
   }
@@ -95,7 +115,49 @@ function dualTransitionToggleOff(elem, str, ov, transitionTime) {
   }
 }
 
-function toggle(elem, classPref, transitionTime, anim, transitions, overlap) {
+// function toggle(elem, classPref, transitionTime, anim, transitions, overlap) {
+//   var selectorPrefix = "__";
+//   if (classPref == "") selectorPrefix = "";
+
+//   classPref = classPref + selectorPrefix;
+
+//   if (anim == "animated") {
+//     var overlapStart = false;
+//     var overlapEnd = false;
+//     if (overlap == "overlap end") {
+//       overlapStart = false;
+//       overlapEnd = true;
+//     } else if (overlap == "overlap start") {
+//       overlapStart = true;
+//       overlapEnd = false;
+//     } else if (overlap == "overlap both") {
+//       overlapStart = true;
+//       overlapEnd = true;
+//     } else {
+//       overlapStart = false;
+//       overlapEnd = false;
+//     }
+
+//     if (transitions != "2 transitions") {
+//       if (elem.classList.contains(classPref + "on")) {
+//         simpleToggleOn(elem, classPref, overlapStart, transitionTime);
+//       } else {
+//         simpleToggleOff(elem, classPref, overlapEnd, transitionTime);
+//       }
+//     } else {
+//       if (elem.classList.contains(classPref + "on")) {
+//         dualTransitionToggleOn(elem, classPref, overlapStart, transitionTime);
+//       } else {
+//         dualTransitionToggleOff(elem, classPref, overlapEnd, transitionTime);
+//       }
+//     }
+//   } else {
+//     elem.classList.toggle(classPref + "on");
+//     elem.classList.toggle(classPref + "off");
+//   }
+// }
+
+function toggle(elem, classPref, transitionTime, anim, transitions, overlap, state) {
   var selectorPrefix = "__";
   if (classPref == "") selectorPrefix = "";
 
@@ -119,23 +181,34 @@ function toggle(elem, classPref, transitionTime, anim, transitions, overlap) {
     }
 
     if (transitions != "2 transitions") {
-      if (elem.classList.contains(classPref + "on")) {
+      if (state === "on") {
         simpleToggleOn(elem, classPref, overlapStart, transitionTime);
-      } else {
+      } else if (state === "off") {
         simpleToggleOff(elem, classPref, overlapEnd, transitionTime);
+      } else if (elem.classList.contains(classPref + "on")) {
+        simpleToggleOff(elem, classPref, overlapEnd, transitionTime);
+      } else {
+        simpleToggleOn(elem, classPref, overlapStart, transitionTime);
       }
     } else {
-      if (elem.classList.contains(classPref + "on")) {
+      if (state === "on") {
         dualTransitionToggleOn(elem, classPref, overlapStart, transitionTime);
-      } else {
+      } else if (state === "off") {
         dualTransitionToggleOff(elem, classPref, overlapEnd, transitionTime);
+      } else if (elem.classList.contains(classPref + "on")) {
+        dualTransitionToggleOff(elem, classPref, overlapEnd, transitionTime);
+      } else {
+        dualTransitionToggleOn(elem, classPref, overlapStart, transitionTime);
       }
     }
   } else {
-    elem.classList.toggle(classPref + "on");
-    elem.classList.toggle(classPref + "off");
+    elem.classList.toggle(classPref + "on", state === "on");
+    elem.classList.toggle(classPref + "off", state === "off");
   }
 }
+
+
+
 
 export default toggle;
 export { simpleToggleOn, simpleToggleOff, dualTransitionToggleOn, dualTransitionToggleOff, toggle };
