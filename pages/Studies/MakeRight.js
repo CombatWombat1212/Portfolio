bdc398eacad6cb54d105b88313e347a2706ad98f
ItsMeSamG <itsmesamg123@gmail.com>
2023-05-31 11:47:49 -0400

    Delivery--Conclusion

--- pages/Studies/MakeRight.js
+++ pages/Studies/MakeRight.js
@@ -42,38 +42,8 @@ function MakeRight({ pop }) {
 
       <CaseStudyPage id={study.id} study={study}>
         
+
         <Chapter name="Delivery" id="Delivery">
-          <Section id="Delivery--Banner-Intro" background={MAKERIGHT_IMGS.delivered_project_banner}>
-            <Heading>Delivered Project</Heading>
-          </Section>
-          <Section type="passthrough" id="Delivery--Maker" background="tertiary" margin="none">
-            {/* TODO: i think maybe these should be the non-tutorial versions :/ what do you think?? */}
-            {/* TODO: yeah i do think these should be the non-tutorial versions */}
-            {/* TODO: Okay so the way you do it is none of them should have tutorials, except one that you think is a really good example of the tutorials.  For that one, show the non-tutorial first, then after it show the tutorial version and comment on what that is */}
-
-            <Slideshow img={MAKERIGHT_IMGS.maker_screen_10}>
-              <Heading>
-                <ResponsiveText tag="Fragment">
-                  <xxl>Maker user journey</xxl>
-                  <lg>Maker journey</lg>
-                </ResponsiveText>
-              </Heading>
-            </Slideshow>
-          </Section>
-
-          <Section type="passthrough" id="Delivery--Customer" background="tertiary" margin="none">
-            <Slideshow img={MAKERIGHT_IMGS.customer_screen_06}>
-              <Heading>
-                <ResponsiveText tag="Fragment">
-                  <xxl>Customer user journey</xxl>
-                  <lg>Customer journey</lg>
-                  <xs>Cust. journey</xs>
-                </ResponsiveText>
-              </Heading>
-            </Slideshow>
-          </Section>
-
-          {/* TODO: next up just finish up this page, making the last component you need */}
 
           <Section id="Delivery--Conclusion" type="columns" titled background="tertiary light">
             <Heading>Bringing 3D printing to everyday consumers</Heading>
@@ -99,59 +69,6 @@ function MakeRight({ pop }) {
             </Column>
           </Section>
 
-          <Section id="Delivery--Forward" type="columns" titled background="tertiary light">
-            <Heading>Going Forward</Heading>
-
-            <Column>
-              <Description>
-                <p>
-                  This project has only just begun. With every feature added, countless more had to be sidelined for the sake of scope. Some of the
-                  planned future additions to the project include:
-                </p>
-                <ul>
-                  <li>
-                    <span>A full branding refresh (the logo and name are temporary)</span>
-                  </li>
-                  <li>
-                    <span>Wider testing with relevant users, both customers and makers</span>
-                  </li>
-                  <li>
-                    <span>Overhauling the visuals and UX of the store page</span>
-                  </li>
-                  <li>
-                    <span>UX of user profiles</span>
-                  </li>
-                  <li>
-                    <span>Real-world tests with owners of 3D printers</span>
-                  </li>
-                </ul>
-              </Description>
-            </Column>
-            <Column>
-              <Description>
-                <p>
-                  Several team members would need to be added in order to become a dedicated company. Like algorithm engineers to help design the
-                  systems that assign orders to makers, and determine how much they get paid per job. Other additions include:
-                </p>
-                <ul>
-                  <li>
-                    <span>More UX designers</span>
-                  </li>
-                  <li>
-                    <span>Wider testing with relevant users, both customers and makers</span>
-                  </li>
-                  <li>
-                    <span>Backend developers (some with cyber-security experience)</span>
-                  </li>
-                  <li>
-                    <span>Financial advisors</span>
-                  </li>
-                </ul>
-              </Description>
-            </Column>
-          </Section>
-
-          <Section id="Delivery--Banner-Closing" background={MAKERIGHT_IMGS["closing_banner"]}></Section>
         </Chapter>
 
 
