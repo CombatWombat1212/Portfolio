98bf84d57324743980ace1eea58029346421ece7
ItsMeSamG <itsmesamg123@gmail.com>
2023-05-31 10:09:51 -0400

    refactoring getSectionChildren and adding new debugging stuff

--- components/sections/sections_utilities/GetSectionChildren.js
+++ components/sections/sections_utilities/GetSectionChildren.js
@@ -1,6 +1,18 @@
 import { DEFINED_CHILDREN } from "../Sections";
 import { getColClassList } from "./GetClasses";
 
+function getComponentName(child) {
+  if (!child || !child.type) return "";
+
+  const { name, displayName } = child.type;
+
+  if (displayName) {
+    return displayName.includes(".") ? displayName.split(".")[0] : displayName;
+  }
+
+  return name || "";
+}
+
 function organizeChildren(allChildren) {
   var allTypes = [...DEFINED_CHILDREN, "Other"];
   var arr = [];
@@ -17,50 +29,98 @@ function organizeChildren(allChildren) {
 
     if (type != "other") {
       arr[i].elems = allChildren.filter((child) => {
-        if (!child.type) return false; // Add this line to check if type is defined
+        if (!child.type) return false; // Check if type is defined
 
-        // Check for both regular component type and mapped component type
-        const componentName =
-          child.type.name || (child.type.displayName || "").split(".")[0];
+        const componentName = getComponentName(child);
+        console.log(componentName);
         return componentName === allTypes[i];
       });
     } else {
       arr[i].elems =
         allChildren.filter((child) => {
-          if (!child.type) return false; // Add this line to check if type is defined
-
-          return (
-            DEFINED_CHILDREN.indexOf(
-              child.type.name || (child.type.displayName || "").split(".")[0]
-            ) === -1
-          );
+          if (!child.type) return false; // Check if type is defined
+          const componentName = getComponentName(child);
+          return DEFINED_CHILDREN.indexOf(componentName) === -1;
         }) || null;
     }
   }
+
   return arr;
 }
 
+// function getSectionChildren(children) {
+//   var allChildren = children;
+
+//   var allTypes = [...DEFINED_CHILDREN, "Other"];
+
+//   var organizedChildren = [];
+
+//   organizedChildren = organizeChildren(allChildren);
+
+//   var [columns, description, title, heading, graphic, quote, other] = [organizedChildren[0].elems, organizedChildren[1].elems, organizedChildren[2].elems, organizedChildren[3].elems, organizedChildren[4].elems, organizedChildren[5].elems, organizedChildren[6].elems];
+
+//   if (columns.length != 0) {
+//     for (var i = 0; i < columns.length; i++) {
+//       var column = columns[i];
+
+//       var columnClasses = getColClassList(column.props.className);
+
+//       var columnChildren = column.props.children;
+//       var organizedColumnChildren = organizeChildren(columnChildren);
+
+//       var [columnColumns, columnDescription, columnTitle, columnHeading, columnGraphic, columnQuote, columnOther] = [organizedColumnChildren[0].elems, organizedColumnChildren[1].elems, organizedColumnChildren[2].elems, organizedColumnChildren[3].elems, organizedColumnChildren[4].elems, organizedColumnChildren[5].elems, organizedColumnChildren[6].elems];
+
+//       columns[i] = {
+//         columns: columnColumns,
+//         description: columnDescription,
+//         title: columnTitle,
+//         heading: columnHeading,
+//         graphic: columnGraphic,
+//         quote: columnQuote,
+//         other: columnOther,
+//         props: {
+//           classes: columnClasses,
+//           ...column.props,
+//         },
+//       };
+//     }
+//   }
+
+//   return { columns, description, title, heading, graphic, quote, other };
+// }
+
 function getSectionChildren(children) {
-  var allChildren = children;
+  // Step 1: Organize children
+  var organizedChildren = organizeChildren(children);
 
-  var allTypes = [...DEFINED_CHILDREN, "Other"];
+  // Step 2: Decompose the organizedChildren object into individual variables
+  var columns = organizedChildren[0].elems;
+  var description = organizedChildren[1].elems;
+  var title = organizedChildren[2].elems;
+  var heading = organizedChildren[3].elems;
+  var graphic = organizedChildren[4].elems;
+  var quote = organizedChildren[5].elems;
+  var other = organizedChildren[6].elems;
 
-  var organizedChildren = [];
-
-  organizedChildren = organizeChildren(allChildren);
-
-  var [columns, description, title, heading, graphic, quote, other] = [organizedChildren[0].elems, organizedChildren[1].elems, organizedChildren[2].elems, organizedChildren[3].elems, organizedChildren[4].elems, organizedChildren[5].elems, organizedChildren[6].elems];
-
+  // Step 3: Handle columns
   if (columns.length != 0) {
     for (var i = 0; i < columns.length; i++) {
-      var column = columns[i];
+      // Handle each column
+      var currentColumn = columns[i];
 
-      var columnClasses = getColClassList(column.props.className);
+      var columnClasses = getColClassList(currentColumn.props.className);
 
-      var columnChildren = column.props.children;
+      var columnChildren = currentColumn.props.children;
       var organizedColumnChildren = organizeChildren(columnChildren);
 
-      var [columnColumns, columnDescription, columnTitle, columnHeading, columnGraphic, columnQuote, columnOther] = [organizedColumnChildren[0].elems, organizedColumnChildren[1].elems, organizedColumnChildren[2].elems, organizedColumnChildren[3].elems, organizedColumnChildren[4].elems, organizedColumnChildren[5].elems, organizedColumnChildren[6].elems];
+      // Decompose the organizedColumnChildren object into individual variables
+      var columnColumns = organizedColumnChildren[0].elems;
+      var columnDescription = organizedColumnChildren[1].elems;
+      var columnTitle = organizedColumnChildren[2].elems;
+      var columnHeading = organizedColumnChildren[3].elems;
+      var columnGraphic = organizedColumnChildren[4].elems;
+      var columnQuote = organizedColumnChildren[5].elems;
+      var columnOther = organizedColumnChildren[6].elems;
 
       columns[i] = {
         columns: columnColumns,
@@ -72,12 +132,13 @@ function getSectionChildren(children) {
         other: columnOther,
         props: {
           classes: columnClasses,
-          ...column.props,
+          ...currentColumn.props,
         },
       };
     }
   }
 
+  // Step 4: Return the organized children
   return { columns, description, title, heading, graphic, quote, other };
 }
 
