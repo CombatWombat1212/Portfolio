4fd0c4726c1974fec87820e510308c722ecfb5be
ItsMeSamG <itsmesamg123@gmail.com>
2023-06-02 11:41:44 -0400

    try this on for size

--- scripts/hooks/useBrowserClass.js
+++ scripts/hooks/useBrowserClass.js
@@ -11,11 +11,24 @@ const useBrowserClass = () => {
     } else if (userAgentString.indexOf("Firefox") > -1) {
       document.documentElement.classList.add('firefox');
     }
-    // ...you can add more conditions for other browsers.
+
+    // Detect OS
+    if (userAgentString.indexOf("Win") !== -1) {
+      document.documentElement.classList.add('windows');
+    } else if (userAgentString.indexOf("Mac") !== -1) {
+      document.documentElement.classList.add('mac');
+    } else if (userAgentString.indexOf("Linux") !== -1) {
+      document.documentElement.classList.add('linux');
+    } else if (userAgentString.indexOf("Android") !== -1) {
+      document.documentElement.classList.add('android');
+    } else if (/iPad|iPhone|iPod/.test(userAgentString) && !window.MSStream) {
+      document.documentElement.classList.add('ios');
+    }
+    
 
     // Cleanup function to remove classes when component is unmounted or conditions change.
     return () => {
-      document.documentElement.classList.remove('chrome', 'safari', 'firefox');
+      document.documentElement.classList.remove('chrome', 'safari', 'firefox', 'windows', 'mac', 'linux', 'android', 'ios');
       // ...you can add more browsers to remove here.
     };
   }, []); // Empty array means this effect runs once on mount and cleanup on unmount.
